bb80dc94a597286ba04abe75503550c53cde155d
Tete Soto <mateusxsoto@gmail.com>
2026-03-16 08:20:25 -0400

    small changes

--- Server Scripts/holdbrakes.lua
+++ Server Scripts/holdbrakes.lua
@@ -42,23 +42,41 @@ DefaultWarning = false
 
 xPos, yPos = ac.getSim().windowWidth,ac.getSim().windowHeight
 
+ac.onOnlineWelcome(function(message, config)     --Reads the script config from the extra options config
+    parsedConfig = tostring(config)
+    configCheck = config:mapSection("HOLDBRAKES", { TARGET_RATE_OF_CHANGE = 0, SAMPLE_TIME = 0, DISPLAY_WARNING_FOR = 0 })
+
+    if configCheck["TARGET_RATE_OF_CHANGE"] == 0 then
+        ac.sendChatMessage("Target ROC Config Missing Or Misconfigured. Falling Back to Defaults")
+    end
+    if configCheck["SAMPLE_TIME"] == 0 then
+        ac.sendChatMessage("Sample Time Config Missing Or Misconfigured. Falling Back to Defaults")
+    end
+    if configCheck["DISPLAY_WARNING_FOR"] == 0 then
+        ac.sendChatMessage("Warning Time Config Missing Or Misconfigured. Falling Back to Defaults")
+    end
+
+    targetRateOfChange = config:get("HOLDBRAKES", "TARGET_RATE_OF_CHANGE", 50)
+    sampleTime = config:get("HOLDBRAKES", "SAMPLE_TIME", 0.5)
+    displayWarningFor = config:get("HOLDBRAKES", "DISPLAY_WARNING_FOR", 5)
+end)
+
 function script.update(dt)
-    
     valid = CAR.isDriftValid
     instantPoints = CAR.driftInstantPoints
-    totalElapsedTime = os.preciseClock() 
+    totalElapsedTime = os.preciseClock()
 
     ac.debug('Instantaneous Points', instantPoints)
     ac.debug('Elapsed totalElapsedTime', totalElapsedTime)
     ac.debug('Points Rate Of Change', pointsRateOfChange)
 
     if pointsRateOfChange > tonumber(targetRateOfChange) then
-        timeWarningStarted = totalElapsedTime  
+        timeWarningStarted = totalElapsedTime
     end
-    
-    if ((timeWarningStarted + tonumber(displayWarningFor)) > totalElapsedTime) and (totalElapsedTime > tonumber(displayWarningFor))  then
-        isWarning = true --Checks the time the warning started at against how long to display it for. 
-    else 
+
+    if ((timeWarningStarted + tonumber(displayWarningFor)) > totalElapsedTime) and (totalElapsedTime > tonumber(displayWarningFor)) then
+        isWarning = true --Checks the time the warning started at against how long to display it for.
+    else
         isWarning = false
     end
 
@@ -70,45 +88,22 @@ function script.update(dt)
         pointsRateOfChange = instantPoints - lastReadPoints
     end
 
-    ac.onOnlineWelcome(function(message, config)  --Reads the script config from the extra options config
-        parsedConfig = tostring(config)
-        configCheck = config:mapSection("HOLDBRAKES", {TARGET_RATE_OF_CHANGE=0,SAMPLE_TIME=0,DISPLAY_WARNING_FOR=0})
-        
-        if configCheck["TARGET_RATE_OF_CHANGE"] == 0 then
-            ac.sendChatMessage("Target ROC Config Missing Or Misconfigured. Falling Back to Defaults")
-        end
-        if configCheck["SAMPLE_TIME"] == 0 then
-            ac.sendChatMessage("Sample Time Config Missing Or Misconfigured. Falling Back to Defaults")            
-        end
-        if configCheck["DISPLAY_WARNING_FOR"] == 0 then
-            ac.sendChatMessage("Warning Time Config Missing Or Misconfigured. Falling Back to Defaults")            
-        end       
-
-        targetRateOfChange = config:get("HOLDBRAKES", "TARGET_RATE_OF_CHANGE", 50)
-        sampleTime = config:get("HOLDBRAKES", "SAMPLE_TIME", 0.5)
-        displayWarningFor = config:get("HOLDBRAKES", "DISPLAY_WARNING_FOR", 5)
-
-        ac.debug("_version", "0.6")
-        ac.debug('asconfig', parsedConfig)
-        ac.debug('BrakeGain', targetRateOfChange)
-        ac.debug('sampleTime', sampleTime)
-        ac.debug('whatever', configChecks)
-
-    end)
-end 
+    ac.debug("_version", "0.7")
+    ac.debug('asconfig', parsedConfig)
+    ac.debug('BrakeGain', targetRateOfChange)
+    ac.debug('sampleTime', sampleTime)
+    ac.debug('whatever', configChecks)
+end
 
 ac.onResolutionChange(function()
-    xPos, yPos = ac.getSim().windowWidth,ac.getSim().windowHeight
+    xPos, yPos = ac.getSim().windowWidth, ac.getSim().windowHeight
 end)
 
 function script.drawUI() --Draws a shitty UI for it.
-
     if isWarning then
-
-        ui.setCursor()
-        ui.dwriteTextAligned("⚠️HOLD YOUR BRAKES⚠️", 0.05*yPos, ui.Alignment.Center, ui.Alignment.Center, vec2(1*xPos,0.4*yPos), false, rgbm.colors.red)
+        ui.dwriteTextAligned("⚠️HOLD YOUR BRAKES⚠️", 0.05 * yPos, ui.Alignment.Center, ui.Alignment.Center,
+            vec2(1 * xPos, 0.4 * yPos), false, rgbm.colors.red)
     end
-
 end
 
     
