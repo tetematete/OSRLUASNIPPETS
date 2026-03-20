6a516e3a0f062a371485e5bc602c13fad8de34c2
Tete Soto <mateusxsoto@gmail.com>
2026-03-20 09:51:07 -0400

    Update acsrtags.lua

--- Server Scripts/acsrtags.lua
+++ Server Scripts/acsrtags.lua
@@ -28,20 +28,33 @@ ac.onOnlineWelcome(function(message, config) --Reads the script config from the 
 end)
 
 ui.onDriverNameTag(false, nil, function(index)
-
-  
   if enabled then
-    local displayTable = ratingsTable["DriverACSRRatings"][timeTable["EntryList"][(ac.getCar(0).sessionID)+1]["GUID"]]
-    ui.drawRectFilled(vec2(420,0),vec2(512,64), rgbm.colors.gray, ui.CornerFlags.All)
-    ui.drawRectFilled(vec2(424,6),vec2(508,30), rgbm.colors.blue, ui.CornerFlags.All)
-    ui.drawRectFilled(vec2(424,35),vec2(508,58), rgbm(0,0.5,0.1,1), ui.CornerFlags.All)
+    --local displayTable = ratingsTable["DriverACSRRatings"][timeTable["EntryList"][(ac.getCar(index).sessionID)+1]["GUID"]]
+    local displayTable = ratingsTable["DriverACSRRatings"][timeTable["EntryList"][index.sessionID + 1]["GUID"]]
 
-    ui.drawTextClipped(displayTable["safety_rating"], vec2(424,31), vec2(508,58), rgbm.colors.white, vec2(0.5,0))
-    ui.drawTextClipped(displayTable["skill_rating_grade"], vec2(424,3), vec2(508,58), rgbm.colors.white, vec2(0.5,0))
-
+    if displayTable ~= nil then
+      ui.drawRectFilled(vec2(420, 0), vec2(512, 64), rgbm.colors.gray, ui.CornerFlags.All)
+      if displayTable["is_provisional"] then
+        ui.drawRectFilled(vec2(424, 6), vec2(508, 58), rgbm.colors.orange, ui.CornerFlags.All)
+        ui.setNextTextBold()
+        ui.pushFont(ui.Font.Tiny)
+        ui.drawTextClipped("Provisional", vec2(424, 6), vec2(508, 58), rgbm.colors.black, vec2(0.5, 0.1))
+        ui.pushFont(ui.Font.Main)
+        ui.setNextTextBold()
+        ui.drawTextClipped(displayTable["num_events"] .. "/8", vec2(424, 6), vec2(508, 58), rgbm.colors.black,
+          vec2(0.5, 0.8))
+      else
+        ui.drawRectFilled(vec2(424, 6), vec2(508, 30), rgbm.colors.blue, ui.CornerFlags.All)
+        ui.drawRectFilled(vec2(424, 35), vec2(508, 58), rgbm(0, 0.5, 0.1, 1), ui.CornerFlags.All)
+        ui.setNextTextBold()
+        ui.drawTextClipped(displayTable["safety_rating"], vec2(424, 31), vec2(508, 58), rgbm.colors.white, vec2(0.5, 0))
+        ui.setNextTextBold()
+        ui.drawTextClipped(displayTable["skill_rating_grade"], vec2(424, 3), vec2(508, 58), rgbm.colors.white,
+          vec2(0.5, 0))
+      end
+    end
   end
-
-end, {tagSize=vec2(512,64)})
+end, { tagSize = vec2(512, 64) })
 
 ui.registerOnlineExtra(ui.Icons.Shield, "ACSR Tags", function ()return true end, function ()
   if ui.checkbox("Enable ACSR Tags", enabled) then
