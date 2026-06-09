412b0d5d971689106479acd92f6ce21f9c177fce
Tete Soto <mateusxsoto@gmail.com>
2026-06-09 12:32:46 -0400

    Update evilAGA.lua

--- Server Scripts/evilAGA.lua
+++ Server Scripts/evilAGA.lua
@@ -1,13 +1,20 @@
-ac.debug("!version", "evilAGA v0.6")
+ac.debug("!version", "evilAGA v0.7")
+ac.debug("csp:", ac.getPatchVersionCode())
+
 if ac.getSim().inputMode ~= ac.UserInputMode.Wheel then
-
-physics.startPhysicsWorker([[
-
-function script.update(dt)
-    physics.overrideSteering(0, ac.getControllerSteerValue())
-end
-
+    if ac.getPatchVersionCode() >= 3978 then
+        physics.startPhysicsWorker([[
+            function script.update(dt)
+                physics.overrideSteering(0, ac.getControllerSteerValue())
+            end
 ]], function(err)
-    ac.log(err)
-    end)
+            ac.log(err)
+        end)
+    else
+        function script.update(dt)
+            physics.overrideSteering(0, ac.getControllerSteerValue())
+        end
+    end
 end
+
+
