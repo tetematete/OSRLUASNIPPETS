016f350540d81d0dbadb92ea0944a4919366c21a
Tete Soto <mateusxsoto@gmail.com>
2026-06-09 12:04:47 -0400

    Update evilAGA.lua

--- Server Scripts/evilAGA.lua
+++ Server Scripts/evilAGA.lua
@@ -1,7 +1,13 @@
+ac.debug("!version", "evilAGA v0.6")
+if ac.getSim().inputMode ~= ac.UserInputMode.Wheel then
+
+physics.startPhysicsWorker([[
+
 function script.update(dt)
-    --ac.debug("input", ac.getControllerSteerValue(),-1,1)
-    --ac.debug("compare", ac.getCar(0).steer/486,-1,1)
-    --ac.debug("table", ac.getGamepadAxisValue(1, ac.GamepadAxis.LeftThumbX))
-    --physics.overrideSteering(0, math.nan)
     physics.overrideSteering(0, ac.getControllerSteerValue())
+end
+
+]], function(err)
+    ac.log(err)
+    end)
 end
