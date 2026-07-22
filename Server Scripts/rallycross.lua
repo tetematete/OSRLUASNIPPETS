e8ca32739a82eb159d5eabbc2d476e8e87b978a6
tetematete <mateusxsoto@gmail.com>
2026-07-22 14:07:02 -0400

    Update rallycross.lua

--- Server Scripts/rallycross.lua
+++ Server Scripts/rallycross.lua
@@ -1,62 +1,102 @@
---=================================================================================================================
---                                  Online Window Maker Class
---=================================================================================================================
---#region
-local onlineWindow = class('onlineWindow')
+    --=================================================================================================================
+    --                                  Online Window Maker Class
+    --=================================================================================================================
+    --#region
+    local onlineWindow = class('onlineWindow')
 
----Makes an interactable window for online scripts.
----@param id string @Window ID, has to be unique within your script.
----@param pos vec2 @Window Initial Position
----@param wsize vec2 @Window size.
----@param noPadding boolean @Disables window padding. Default value: `false`.
----@param transparent boolean @Whether window should be transparent
----@overload fun(id: string, pos: vec2, wsize: vec2, noPadding: boolean, transparent: boolean)
-function onlineWindow.initialize(self, id, pos, wsize, noPadding, transparent)
-    self.id = const(id)
-    self.winSettings = ac.storage({
-        windowPos = pos,
-        windowSize = wsize
-    }, self.id)
-    self.transparent = const(transparent)
-    self.noPadding = const(noPadding)
+    ---Makes an interactable window for online scripts.
+    ---@param id string @Window ID, has to be unique within your script.
+    ---@param pos vec2 @Window Initial Position
+    ---@param wsize vec2 @Window size.
+    ---@param noPadding boolean @Disables window padding. Default value: `false`.
+    ---@param transparent boolean @Whether window should be transparent
+    ---@overload fun(id: string, pos: vec2, wsize: vec2, noPadding: boolean, transparent: boolean)
+    function onlineWindow.initialize(self, id, pos, wsize, noPadding, transparent)
+        self.id = const(id)
+        self.winSettings = ac.storage({
+            windowPos = pos,
+            windowSize = wsize
+        }, self.id)
+        self.transparent = const(transparent)
+        self.noPadding = const(noPadding)
 
-    self.size = const(wsize)
-    self.dragging = false
-    self.startPos = vec2()
-    self.move = function ()
-        if ui.windowHovered() then
-            if ui.isMouseDragging(ui.MouseButton.Left) and not self.dragging --[[and not betterFlagSettings.flagWindowPinned ]]then
-                self.startPos = ui.windowPos()
-                 self.dragging = true
-            end            
+        self.size = const(wsize)
+        self.dragging = false
+        self.startPos = vec2()
+        self.move = function ()
+            if ui.windowHovered() then
+                if ui.isMouseDragging(ui.MouseButton.Left) and not self.dragging --[[and not betterFlagSettings.flagWindowPinned ]]then
+                    self.startPos = ui.windowPos()
+                    self.dragging = true
+                end            
+            end
+            if self.dragging and ui.mouseDragDelta(ui.MouseButton.Left) ~= vec2(0, 0) then
+                self.winSettings.windowPos = self.startPos + ui.mouseDragDelta()
+            else
+                self.dragging = false
+            end
         end
-        if self.dragging and ui.mouseDragDelta(ui.MouseButton.Left) ~= vec2(0, 0) then
-            self.winSettings.windowPos = self.startPos + ui.mouseDragDelta()
-        else
-            self.dragging = false
+        self.window = function(s, content)
+            
+            if s.transparent then
+                ui.transparentWindow(s.id, s.winSettings.windowPos, self.size, self.noPadding, true, function ()
+                    self.move()
+                    content()
+                end)
+            else
+                ui.toolWindow(s.id, s.winSettings.windowPos, self.size,self.noPadding, true, function ()
+                    self.move()
+                    content() 
+                end)
+            end
         end
-    end
-    self.window = function(s, content)
         
-        if s.transparent then
-            ui.transparentWindow(s.id, s.winSettings.windowPos, self.size, self.noPadding, true, function ()
-                self.move()
-                content()
-            end)
-        else
-            ui.toolWindow(s.id, s.winSettings.windowPos, self.size,self.noPadding, true, function ()
-                self.move()
-                content() 
-            end)
+    end
+    onlineWindow.make = class.emmy(onlineWindow, onlineWindow.initialize)
+    --#endRegion
+
+    --=================================================================================================================
+    --                                  Spline Maker Class
+    --=================================================================================================================
+    --#reigon
+local cubic = require('shared/math/cubic')
+local splineContainer = class('splineContainer')
+local splinePoint = class('splinePoint')
+function splineContainer:initialize(size,dist,age,rotation,color)
+    self.size = size or 1
+    self.dist = dist or 1
+    self.age = age or 0.5
+    self.rotation = rotation or 90
+    self.color = color or rgbm.colors.white
+    self.pts = {}
+    self.paint = ac.TrackPaint()
+    self.collectedPoints = 0
+
+    self.createPoint = function(pos,size)
+        self.pts[#self.pts+1] = splinePoint(pos,size)
+    end
+    self.deletePoint = function (index)
+        table.remove(self.pts, index)
+    end
+    self.reset = function ()
+        self.collectedPoints = 0
+        for index, pt in ipairs(self.pts) do
+            pt.collected = false
         end
     end
-    
 end
-onlineWindow.make = class.emmy(onlineWindow, onlineWindow.initialize)
---#endRegion
 
-local cubic = require('shared/math/cubic')
-ac.debug("!version", "jokerlap v0.2")
+function splinePoint:initialize(pos, size)
+    self.pos = pos
+    self.size = size or 5
+    self.helper = render.PositioningHelper({ skipAxis = {'y'} })
+    self.collected=false
+end
+
+
+    --#endRegion
+
+ac.debug("!version", "jokerlap v0.3")
 
 --If you intend to modify this script, leave these in. 
 ac.debug("URL", "https://github.com/tetematete/OSRLUASNIPPETS/tree/main")
@@ -85,7 +125,7 @@ local offset = 0
 local size = 1
 local dist = 1
 local age = 0.5
-local spl = {}
+--local spl = {}
 local noVisuals = false
 local arrowColor = rgbm(0.4, 0.8, 1, 0)
 local active = false
@@ -121,6 +161,7 @@ end
 --==============================================================================
 --      Load server spline settings
 --==============================================================================
+local jSpl = splineContainer()
 ac.onOnlineWelcome(function(message, config)
     --config = testa
     local sec = "RX_CONFIG"
@@ -129,8 +170,8 @@ ac.onOnlineWelcome(function(message, config)
 
      local sec = "RALLYCROSS"
     for index, key in config:iterateValues(sec, "POINT", true) do
-        local pos = config:get(sec, key, vec3())
-        table.insert(spl, { pos = pos, helper = render.PositioningHelper({ skipAxis = { 'y' } }), collected=false })
+        local pos = config:get(sec, key, vec4())
+        jSpl.createPoint(vec3(pos.x, pos.y, pos.z), pos.w)
     end
     hitbox = config:get(sec, "HITBOX", 2)
     offset = config:get(sec, "OFFSET", 0)
@@ -140,11 +181,6 @@ ac.onOnlineWelcome(function(message, config)
     rotation = config:get(sec, "ROTATION", 90 - ac.getRealTrackHeadingAngle())
     arrowColor = config:get(sec, "COLOR", rgbm(0.4, 0.8, 1, 0))
 
-    if #spl > 3 then
-        makePaint()
-    end
-
-
     active = true
 end)
 
@@ -152,6 +188,8 @@ end)
 --==============================================================================
 --          Spline Creation and Export Tool
 --==============================================================================
+
+
 ui.registerOnlineExtra(ui.Icons.Pitlane, "RALLYCROSS", nil, function()
     local offtrue, hittrue, sizetrue, disttrue, agetrue,rotationtrue = false, false, false, false, false, false
     showDebug = true
@@ -160,65 +198,54 @@ ui.registerOnlineExtra(ui.Icons.Pitlane, "RALLYCROSS", nil, function()
     ui.columns(2, true, "Columntest")
     
   --if ui.checkbox("No Visuals", noVisuals) then noVisuals = not noVisuals makePaint() end
-    hitbox, hittrue = ui.slider("hitbox", hitbox, 0.5, 3)
-    size, sizetrue = ui.slider("size", size, 0.5, 3)
-    dist, disttrue = ui.slider("dist", dist, 0.01, 3)
-    age, agetrue = ui.slider("age", age, 0, 1)
-    rotation, rotationtrue = ui.slider("rotation", rotation, 0, 360)
-    if sizetrue or disttrue or agetrue or rotationtrue then
-        makePaint()
-    end
+    --hitbox, hittrue = ui.slider("hitbox", hitbox, 1, 20, '%.1f')
+    --size, sizetrue = ui.slider("size", size, 0.5, 3, '%.1f')
+    --dist, disttrue = ui.slider("dist", dist, 0.01, 3, '%.1f')
+    --age, agetrue = ui.slider("age", age, 0, 1, '%.1f')
+    --rotation, rotationtrue = ui.slider("rotation", rotation, 0, 360, '%.1f')
+
+
     if ui.hotkeyShift() and ui.mouseClicked(ui.MouseButton.Left) then
         local ray = render.createMouseRay()
         local pos = vec3()
         if physics.raycastTrack(ray.pos, ray.dir, math.huge, pos) ~= -1 then
-            ac.log(pos)
-            table.insert(spl, { pos = pos, helper = render.PositioningHelper({ skipAxis = {'y'} }), collected=false })
-            if #spl > 3 then
-                makePaint()
-            end
+            jSpl.createPoint(pos, 5)
+            ui.storeNumber(0, #jSpl.pts)
+        end
+    end
+    
+
+
+    for index, value in ipairs(jSpl.pts) do
+        if ui.menuItem(index, ui.loadStoredNumber(0,1)==index) then
+            ui.storeNumber(0, index)
         end
     end
     
     if ui.button("Export Current") then
         local sec = "RALLYCROSS"
         local config = ac.INIConfig(ac.INIFormat.Extended, {[sec]={}})
-        for index, value in ipairs(spl) do
-            config:set(sec, "POINT_" .. index-1, value.pos)
+        for index, value in ipairs(jSpl.pts) do
+            config:set(sec, "POINT_" .. index-1, vec4(value.pos.x,value.pos.y,value.pos.z,value.size))
         end
-        config:set(sec, "HITBOX", hitbox)
-        --config:set("ATTACKMODE", "NO_VISUALS", noVisuals)
-        --config:set("ATTACKMODE", "OFFSET", offset)
-        config:set(sec,"COLOR", arrowColor)
-        config:set(sec, "SIZE", size)
-        config:set(sec, "DIST", dist)
-        config:set(sec, "AGE", age)
-        config:set(sec, "ROTATION", rotation)
+
         ac.log(config:serialize())
         ac.setClipboardText(config:serialize())
         tempConfig = config:serialize()
     end
 
-    for index, value in ipairs(spl) do
-        ui.text(index .. ": " .. tostring(value.pos))
-        if ui.itemHovered(ui.HoveredFlags.None) then
-            ui.itemUnderline()
-        end
+        ui.text("shift + click \nto add point")
 
-        if ui.itemClicked(ui.MouseButton.Left) then
-            table.remove(spl, index)
-            makePaint()
-        end
-    end
-    
-
-    if #spl > 3 then
-    else
-        ui.text("4 Points Required, shift + click to add a point")
-    end
 ui.nextColumn()
-if ui.colorPicker("Arrow Color", arrowColor, ui.ColorPickerFlags.AlphaBar) then makePaint() end
+--if ui.colorPicker("Arrow Color", arrowColor, ui.ColorPickerFlags.AlphaBar) then makePaint() end
+if #jSpl.pts > 0 then 
+    jSpl.pts[ui.loadStoredNumber(0, 1)].size = ui.slider("hitbox", jSpl.pts[ui.loadStoredNumber(0, 1)].size, 1, 20, '%.1f')
+end
 
+if ui.button("Delete Point") then
+    jSpl.deletePoint(ui.loadStoredNumber(0, 0))
+    ui.storeNumber(0, #jSpl.pts)
+end
     ui.nextColumn()
     ui.columns(1)
     ui.separator()
@@ -236,21 +263,21 @@ end, bit.bor(ui.OnlineExtraFlags.Admin, ui.OnlineExtraFlags.Tool))
 local helperGrabbed = false
 function script.draw3D()
     if showDebug then
-        for index, val in ipairs(spl) do
+        for index, val in ipairs(jSpl.pts) do
             local track = ac.worldCoordinateToTrack(val.pos)
             track.x = track.x + offset
-            render.debugSphere(ac.trackCoordinateToWorld(track), hitbox, val.collected and rgbm.colors.green or nil)
+            render.debugSphere(ac.trackCoordinateToWorld(track), val.size, val.collected and rgbm.colors.green or nil)
             
         end
-        for index, val in ipairs(spl) do
+        for index, val in ipairs(jSpl.pts) do
             val.helper:render(val.pos)
             render.debugText(val.pos, index)
         end
         
         render.debugText(car.position, "Car Hitbox")
-        if not render.isPositioningHelperBusy() and helperGrabbed and #spl > 3 then
+        --[[if not render.isPositioningHelperBusy() and helperGrabbed and #jSpl.pts > 3 then
             makePaint()
-        end
+        end]]
 
         helperGrabbed = render.isPositioningHelperBusy()
     end
@@ -259,20 +286,21 @@ end
 --==============================================================================
 -- Script Constant update Logic
 --==============================================================================
-local collectedPoints = 0
+--local collectedPoints = 0
 local jokerStatus = 0
 function script.update(dt)
+    ac.debug("A", jSpl.pts)
 --ac.debug("a", car.p2pStatus)
-    if active and not (#spl == 0) then
-        if collectedPoints == #spl then
+    if active and not (#jSpl.pts == 0) then
+        if jSpl.collectedPoints == #jSpl.pts then
             allCollected()
         else
-            if collectedPoints < #spl and spl[collectedPoints + 1].pos:distance(car.position) < hitbox then
-                if collectedPoints == 0 and jokerStatus ~= 2 then
+            if jSpl.collectedPoints < #jSpl.pts and jSpl.pts[jSpl.collectedPoints + 1].pos:distance(car.position) < jSpl.pts[jSpl.collectedPoints + 1].size then
+                if jSpl.collectedPoints == 0 and jokerStatus ~= 2 then
                     bcast({status=1})
                 end
-                spl[collectedPoints + 1].collected = true
-                collectedPoints = collectedPoints + 1
+                jSpl.pts[jSpl.collectedPoints + 1].collected = true
+                jSpl.collectedPoints = jSpl.collectedPoints + 1
             end
         end
 
@@ -341,8 +369,8 @@ function script.drawUI()
         progress = jsmooth(0)
         progress2 = 0
     elseif jokerStatus == 1 then
-        progress = jsmooth(400 * (collectedPoints / #spl))
-        progress2 = 400 * (collectedPoints / #spl)
+        progress = jsmooth(400 * (jSpl.collectedPoints / #jSpl.pts))
+        progress2 = 400 * (jSpl.collectedPoints / #jSpl.pts)
     else
         progress = jsmooth(400)
         progress2 = 400
@@ -414,11 +442,12 @@ bcast = ac.OnlineEvent({
     end
 end)
 
+---@param t table @The table that you would normally pass to the function returned by ac.onlineEvent
 function castStatus(t)
     math.randomseed(sim.currentSessionTime)
-    if not bcast(t, true) then
+    if not bcast(t) then
         setInterval(function ()
-            if bcast(t, true) then
+            if bcast(t) then
                 return clearInterval
             end
         end, math.random())
@@ -460,12 +489,9 @@ ac.onLapCompleted(0, function (carIndex, lapTime, valid, cuts, lapCount)
 end)
 
 function resetCollected()
-    collectedPoints = 0
+    jSpl.reset()
     if jokerStatus ~= 2 then
             bcast({status=0})
-    end
-    for index, point in ipairs(spl) do
-        point.collected = false
     end
 end
 
