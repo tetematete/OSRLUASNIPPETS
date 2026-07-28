7953a7a5c8eee1880e874287463c567f4d1f0ab1
tetematete <mateusxsoto@gmail.com>
2026-07-28 11:22:12 -0400

    Update realDRS.lua

--- Server Scripts/realDRS.lua
+++ Server Scripts/realDRS.lua
@@ -8,66 +8,77 @@ local gapAhead = 1
 local flipAllowed = false
 local DRSEnabled = activateOnLap == 0 and true or false
 local areWeFR = const(true)
-
+local drsData = {}
+ac.debug("!version", "realDRS v0.2")
 for index, section in drsZones:iterate("ZONE") do
-    local drsData = drsZones:mapSection(section, { DETECTION = 0, START = 0, END = 0 })
-    crossingTimes[index] = {}
-    for i, c in ac.iterateCars() do
-        crossingTimes[index][i] = 0
-    end
-
-    ac.onTrackPointCrossed(-1, drsData.DETECTION, function(carIndex, timeMs)
-        if DRSEnabled then
-            if carIndex ~= 0 then
-                crossingTimes[index][carIndex+1] = timeMs
+    drsData[index] = drsZones:mapSection(section, { DETECTION = 0, START = 0, END = 0, TIMEOUT = -1, ALLOWED = true })
+    ac.log(drsData[index])
+    ac.log("DRS: " .. section)
+    ac.onTrackPointCrossed(-1, drsData[index].DETECTION, function(carIndex, timeMs)
+        if carIndex ~= 0 then
+            drsData[index].ALLOWED = true
+            clearTimeout(drsData[index].TIMEOUT)
+            drsData[index].TIMEOUT = setTimeout(function()
+                drsData[index].ALLOWED = false
+            end, gapAhead)
+        else
+            if drsData[index].ALLOWED and sim.leaderLapCount >= activateOnLap and sim.raceSessionType == ac.SessionType.Race then
+                physics.allowCarDRS(0, areWeFR)
+                ac.log("DRS: Gap ahead under threshold, enabling DRS.")
             else
-                for i, v in ipairs(crossingTimes[index]) do
-                    if timeMs - v < gapAhead*1000 then
-                        physics.allowCarDRS(0, not areWeFR)
-                        ac.log("Gap ahead under 1 second. Allowing DRS")
-                        break
-                    else
-                        physics.allowCarDRS(0, areWeFR)
-                    end
-                end
-
+                physics.allowCarDRS(0, not areWeFR)
+                ac.log("DRS: Gap ahead above threshold or not yet enabled, disabling DRS.")
             end
         end
     end)
 end
-if crossingTimes == {} then
+--physics.setCarAutopilot(true)
+
+if drsData == {} then
     ac.log("No or Malformed drs_zones.ini detected!")
 end
 
-ac.onOnlineWelcome(function (message, config)
+ac.onOnlineWelcome(function(message, config)
     local sec = "REALDRS"
     activateOnLap = config:get(sec, "ACTIVE_ON_LAP", 1)
     gapAhead = config:get(sec, "GAP_AHEAD", 1)
-    flipAllowed = config:get(sec, "FLIP_ALLOWED", false)
+    flipAllowed = config:get(sec, "FLIP_ALLOWED", true)
     if flipAllowed then
         areWeFR = not areWeFR
     end
+
+    ac.debug("Config:",
+        "ACTIVE_ON_LAP=" .. activateOnLap .. "\nGAP_AHEAD=" .. gapAhead .. "\nTRUE = " .. (areWeFR and "TRUE" or "FALSE"))
 end)
 
-ac.onTrackPointCrossed(-1, 1, function (carIndex, timeMs)
-    if sim.leaderLapCount >= activateOnLap and sim.raceSessionType == ac.SessionType.Race then
-        DRSEnabled = true
-    end
+
+ac.onLapCompleted(-1, function(carIndex, lapTime, valid, cuts, lapCount)
+    setTimeout(function()
+        if sim.leaderLapCount >= activateOnLap and sim.raceSessionType == ac.SessionType.Race then
+            if not DRSEnabled then
+                ac.log("DRS: NOW ENABLED")
+            end
+            DRSEnabled = true
+        end
+    end, 1)
 end)
 
 local started = false
-ac.onSessionStart(function (sessionIndex, restarted)
+ac.onSessionStart(function(sessionIndex, restarted)
+    ac.log("DRS: SESSION RESTARTED\nDRS DISABLED")
+    physics.allowCarDRS(0, areWeFR)
     started = false
+    DRSEnabled = false
 end)
 
-function script.update(dt)
-    if not started then
+
+
+--[=[function script.update(dt)
+    ac.debug("drsData", drsData)
+    ac.debug("d", car.drsAvailable)
+    --[[if not started then
         if sim.isSessionStarted then
-            if sim.raceSessionType == ac.SessionType.Race then
-                ac.log("Disabling DRS")
-                physics.allowCarDRS(0, areWeFR)
-            end
             started = true
         end
-    end
-end
+    end]]
+end]=]
