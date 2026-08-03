ffb28914346187d1ac654c92274c9b257596cb10
Tete Soto <mateusxsoto@gmail.com>
2026-08-03 14:35:14 -0400

    Update forceCSPVersion.lua

--- Server Scripts/forceCSPVersion.lua
+++ Server Scripts/forceCSPVersion.lua
@@ -1,16 +1,27 @@
-ac.debug("!version", "forceCSPVersion v1.0")
+ac.debug("!version", "forceCSPVersion v1.1")
 
 --If you intend to modify this script, leave these in. 
 ac.debug("URL", "https://github.com/tetematete/OSRLUASNIPPETS/tree/main")
 ac.debug("Credit", "original script by tetematete, co-owner of OSR. \nTo race with us, support us, or find more scripts like this one,\n follow the link below.")
 local sim = ac.getSim()
-local ver = 0
+local ver = {}
 local explodePlayer = false
+local versionCorrect = false
 
 ac.onOnlineWelcome(function (message, config)
     local sec = "CSPVERSION"
-    ver = config:get("CSPVERSION", "VERSION", ac.INIConfig.OptionalNumber)
-    if ac.getPatchVersionCode() == ver then
+    for i = 1, 99, 1 do
+        ver[i] = config:get(sec, "VERSION", ac.INIConfig.OptionalNumber, i)
+    end
+
+    for index, version in ipairs(ver) do
+        if version == ac.getPatchVersionCode() then
+            versionCorrect = true
+        end
+    end
+    
+
+    if versionCorrect then
         ac.log("Version Correct")
         function script.drawUI(dt)
         end
@@ -32,7 +43,7 @@ function script.drawUI(dt)
     if explodePlayer then
     ui.drawRectFilled(0, ui.windowSize(), rgbm(1, 0, 0, 0.3))
     ui.dwriteTextAligned(
-    "Incorrect CSP Version.\nExpected: " .. ver .. "\nActual: " .. ac.getPatchVersionCode(), 50,
+    "Incorrect CSP Version.\nExpected: " .. table.concat(ver, ', ') .. "\nActual: " .. ac.getPatchVersionCode(), 50,
         ui.Alignment.Center, ui.Alignment.Center, ui.windowSize(), false, rgbm.colors.yellow)
     end
 end
